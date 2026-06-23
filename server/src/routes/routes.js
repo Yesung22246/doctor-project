64eefa38ffa2fe91ef0b75f5e5db992c81b0f950
hoang22042006 <hoangnguyenduc03@gmail.com>
2026-06-24 00:10:23 +0700

    fixed v7

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,9 +1,10 @@
 // import các controller 
 const userRoute = require("../routes/route/userRoute")
-
+const doctorController = require("../controllers/DoctorController");
 function router(app){ 
     app.use("/api/users",  userRoute)
     app.use("/api/doctors",  require("../routes/route/doctorRoute"))
+    app.get("/api/doctors/seed", doctorController.seedDoctors);
     app.use("/api/bookings",  require("../routes/route/bookingRoute"))
     // app.use("/haha",  controller)
     // app.use("/haha",  controller)
